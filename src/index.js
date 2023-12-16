82c05cb197acee58d98f2c7ecf7683a05c2f75a8
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-17 04:07:09 +0600

    signup successfully in server

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const env = require("dotenv");
 const app = express();
-
 const mongoose = require('mongoose');
-
+const UserRouter = require("./routes/userRoutes");
 // Environment setup
 env.config();
+
+//routes
+
+
+
 
 // Connect to MongoDB
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.yrorxmn.mongodb.net/${process.env.MONGODB_DATABASE_URL}?retryWrites=true&w=majority`)
     .then(() => console.log('Database connected!'))
     .catch((err) => console.error('Database connection error:', err));
 
-console.log(process.env.MONGODB_DATABASE_URL);
-
 // Middleware to parse incoming request bodies
 app.use(express.json()); // To parse JSON bodies
-app.use(express.urlencoded({ extended: true })); // To parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+//routes
+app.use('/api', UserRouter);
 
 app.get("/", (req, res, next) => {
     res.status(200).json({
@@ -24,11 +28,7 @@ app.get("/", (req, res, next) => {
     });
 });
 
-app.post("/data", (req, res, next) => {
-    res.status(200).json({
-        message: req.body
-    });
-});
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
