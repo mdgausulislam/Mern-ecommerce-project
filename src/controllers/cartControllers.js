cf3b2fa9a21b0074215385964e83066b9bbc44f4
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-19 03:47:58 +0600

    add to the cart all product

--- src/controllers/cartControllers.js
+++ src/controllers/cartControllers.js
@@ -1,20 +1,37 @@
-
 const Cart = require('../models/cartModel');
 
 const addItemToCart = async (req, res) => {
-    //    return res.status(200).json({ message: 'cart' })
+    try {
+        const existingCart = await Cart.findOne({ user: req.user._id });
 
-    const cart = new Cart({
-        user: req.user._id,
-        cartItems: req.body.cartItems,
+        if (existingCart) {
+            const product = req.body.cartItems.product;
+            const item = existingCart.cartItems.find(c => c.product === product);
 
-    })
+            if (item) {
+                // If item already exists in cart, update the quantity
+                item.quantity += req.body.cartItems.quantity;
+            } else {
+                // If item does not exist in cart, add it to the cartItems array
+                existingCart.cartItems.push(req.body.cartItems);
+            }
 
-    const newCartItem = new Cart(cart);
-    const saveCartItem = await newCartItem.save();
-    res.status(201).json({ cart: saveCartItem })
+            // Save the updated cart
+            const updatedCart = await existingCart.save();
+            return res.status(200).json({ cart: updatedCart });
+        } else {
+            // If cart does not exist, create a new cart for the user
+            const newCart = new Cart({
+                user: req.user._id,
+                cartItems: [req.body.cartItems],
+            });
 
-}
+            const savedCart = await newCart.save();
+            return res.status(201).json({ cart: savedCart });
+        }
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
 
-
-module.exports = { addItemToCart }
+module.exports = { addItemToCart };
