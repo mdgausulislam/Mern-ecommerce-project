806bd8e3cbfbb9a4f6e2dd39ff075dde156c2f47
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-19 04:10:00 +0600

    update add to cart  and product error solve

--- src/controllers/cartControllers.js
+++ src/controllers/cartControllers.js
@@ -5,20 +5,34 @@ const addItemToCart = async (req, res) => {
         const existingCart = await Cart.findOne({ user: req.user._id });
 
         if (existingCart) {
+            // If cart exists for the user, update the cart items
+
             const product = req.body.cartItems.product;
-            const item = existingCart.cartItems.find(c => c.product === product);
+            const item = existingCart.cartItems.find(c => c.product == product);
 
             if (item) {
-                // If item already exists in cart, update the quantity
-                item.quantity += req.body.cartItems.quantity;
+                await Cart.findOneAndUpdate(
+                    { user: req.user._id, "cartItems.product": product },
+                    {
+                        $set: {
+                            "cartItems.$.quantity": item.quantity + req.body.cartItems.quantity
+                        }
+                    }
+                );
             } else {
-                // If item does not exist in cart, add it to the cartItems array
-                existingCart.cartItems.push(req.body.cartItems);
+                await Cart.findOneAndUpdate(
+                    { user: req.user._id },
+                    {
+                        $push: {
+                            cartItems: req.body.cartItems
+                        }
+                    }
+                );
             }
 
-            // Save the updated cart
-            const updatedCart = await existingCart.save();
+            const updatedCart = await Cart.findOne({ user: req.user._id });
             return res.status(200).json({ cart: updatedCart });
+
         } else {
             // If cart does not exist, create a new cart for the user
             const newCart = new Cart({
