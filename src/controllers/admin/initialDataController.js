25c7a54f5d61ec9c4c16c484abca3472ddcc9002
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-22 04:08:53 +0600

    initial all deta create in server api

--- src/controllers/admin/initialDataController.js
+++ src/controllers/admin/initialDataController.js
@@ -1,14 +1,38 @@
 const categoryModel = require("../../models/categoryModel");
 const productModel = require("../../models/productModel");
 
+
+function createCategories(categories, parentId = null) {
+    const categoryList = [];
+    let category;
+    if (parentId == null) {
+        category = categories.filter((cat) => cat.parentId == undefined)
+    } else {
+        category = categories.filter((cat) => cat.parentId == parentId)
+    }
+
+    for (let cate of category) {
+        categoryList.push({
+            _id: cate._id,
+            name: cate.name,
+            slug: cate.slug,
+            parentId: cate.parentId,
+            children: createCategories(categories, cate._id)
+        });
+    }
+    return categoryList;
+}
+
 const initialDataController = async (req, res) => {
     try {
         const categories = await categoryModel.find({}).exec();
         console.log(categories); // Logging the retrieved categories
-        const products = await productModel.find({}).exec();
+        const products = await productModel.find({})
+            .select('_id name price quantity slug description productPicture category')
+            .exec();
         console.log(products); // Logging the retrieved products
         res.status(200).json({
-            categories,
+            categories: createCategories(categories),
             products,
         });
     } catch (error) {
