7d325992b0d92b131d1761cf9d3f8718c8c7e349
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-24 02:47:15 +0600

    product list items

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,6 @@
 const slugify = require('slugify');
 const Product = require('../models/productModel');
+const categoryModel = require('../models/categoryModel');
 
 const CreateProduct = async (req, res) => {
     const { name, price, description, category, quantity, createdBy } = req.body;
@@ -34,10 +35,38 @@ const CreateProduct = async (req, res) => {
 
 
 const getProductsBySlug = async (req, res) => {
-    const { slug } = req.params;
 
-    res.status(200).json({ slug });
+    const { slug } = req.params;
+    console.log("Received slug:", slug);
+
+    const category = await categoryModel.findOne({ slug: { $regex: new RegExp(slug, 'i') } }).select('_id').exec();
+    console.log("Retrieved category:", category);
+
+    if (!category) {
+        console.log("Category not found");
+        return res.status(404).json({ error: "Category not found" });
+    }
+
+    let products = await Product.find({ category: category._id }).exec();
+
+    if (products.length > 0) {
+        const productByPrice = {
+            under5k: products.filter(product => product.price <= 5000),
+            under10k: products.filter(product => product.price > 5000 && product.price <= 10000),
+            under15k: products.filter(product => product.price > 10000 && product.price <= 15000),
+            under20k: products.filter(product => product.price > 15000 && product.price <= 20000),
+            under30k: products.filter(product => product.price > 20000 && product.price <= 30000),
+        };
+
+        res.status(200).json({
+            products,
+            productByPrice,
+        });
+    } else {
+        res.status(200).json({ products: [], productByPrice: {} });
+    }
 };
+
 
 
 
