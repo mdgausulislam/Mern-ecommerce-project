12b408bd7597ee53b3746d3cb8286f5fa63f04e2
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-27 01:19:10 +0600

    error solve admin page

--- src/controllers/admin/adminPageController.js
+++ src/controllers/admin/adminPageController.js
@@ -19,10 +19,11 @@ const createPage = async (req, res) => {
         req.body.createdBy = req.user._id;
 
         let page = await Page.findOne({ category: req.body.category });
-
+    
         if (page) {
             page = await Page.findOneAndUpdate({ category: req.body.category }, req.body, { new: true });
             return res.status(201).json({ page });
+
         } else {
             const newPage = new Page(req.body);
             await newPage.save();
@@ -37,9 +38,6 @@ const createPage = async (req, res) => {
 const getPage = async (req, res) => {
     try {
         const { category, type } = req.params;
-        console.log("category:", category);
-        console.log('type:', type);
-
         if (type === "page") {
             const page = await Page.findOne({ category }).exec();
             console.log("page received:", page);
@@ -61,44 +59,3 @@ const getPage = async (req, res) => {
 
 module.exports = { createPage, getPage };
 
-
-
-// const Page = require("../../models/pageModel");
-
-// const createPage = async (req, res) => {
-//     const { banners, products } = req.files;
-//     try {
-//         if (!req.user || !req.user._id) {
-//             return res.status(400).json({ error: "User not authenticated properly" });
-//         }
-//         if (banners && banners.length > 0) {
-//             req.body.banners = banners.map((banner) => ({
-//                 img: `${process.env.API}/public/${banner.filename}`,
-//                 navigateTo: `/bannerClicked?categoryId=${req.body.category}&type=${req.body.type}`,
-//             }));
-//         }
-//         if (products && products.length > 0) {
-//             req.body.products = products.map((product) => ({
-//                 img: `/public/${product.filename}`,
-//                 navigateTo: `/productClicked?categoryId=${req.body.category}&type=${req.body.type}`,
-//             }));
-//         }
-
-//         req.body.createdBy = req.user._id;
-
-//         let page = await Page.findOne({ category: req.body.category });
-
-//         if (page) {
-//             page = await Page.findOneAndUpdate({ category: req.body.category }, req.body, { new: true });
-//             return res.status(201).json({ page: updatedPage });
-//         } else {
-//             page = new Page(req.body);
-//             await page.save();
-//             return res.status(201).json({ page });
-//         }
-//     } catch (error) {
-//         return res.status(400).json({ error: error.message });
-//     }
-// };
-
-// module.exports = { createPage };
