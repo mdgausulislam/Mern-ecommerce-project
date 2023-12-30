75bcc99a9847bf8920e45d490f85163a0bdf7720
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-30 17:40:58 +0600

    cart get items api

--- src/controllers/cartControllers.js
+++ src/controllers/cartControllers.js
@@ -58,7 +58,35 @@ const addItemToCart = async (req, res) => {
     }
 };
 
-module.exports = { addItemToCart };
+
+
+const getCartItems = async (req, res) => {
+    try {
+        const cart = await Cart.findOne({ user: req.user._id })
+            .populate("cartItems.product", "_id name price productPictures")
+            .exec();
+
+        if (cart) {
+            let cartItems = {};
+            cart.cartItems.forEach((item, index) => {
+                cartItems[item.product._id.toString()] = {
+                    _id: item.product._id.toString(),
+                    name: item.product.name,
+                    img: item.product.productPictures[0].img,
+                    price: item.product.price,
+                    qty: item.quantity,
+                };
+            });
+            res.status(200).json({ cartItems });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+
+module.exports = { addItemToCart, getCartItems };
+
 
 
 
--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { addItemToCart } = require("../controllers/cartControllers");
+const { addItemToCart, getCartItems } = require("../controllers/cartControllers");
 const verifyToken = require("../middleware/verifyToken");
 const verifyUser = require("../middleware/verifyUser");
 
@@ -8,7 +8,7 @@ const verifyUser = require("../middleware/verifyUser");
 
 
 router.post('/user/cart/addtocart', verifyToken, verifyUser, addItemToCart)
-// router.post("/user/getCartItems", verifyToken, verifyUser, getCartItems);
+router.post("/user/getCartItems", verifyToken, verifyUser, getCartItems);
 // router.get('/category/getcategory', getCategory)
 
 module.exports = router;
